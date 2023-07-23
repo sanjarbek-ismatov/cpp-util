1a83a5964b932c7958c8b407cc34ca3f3a796def
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-23 19:09:47 +0500

    formatting

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <string>
 #include "utils/search.hpp"
+
 using namespace std;
-int main(){
-    vector<int> arr = {4, 4, 6, 2, 6, 7, 3, 1, 7, 8};
+int main() {
+    vector < int > arr = {4, 4, 6, 2, 6, 7, 3, 1, 7, 8};
     Search searchUtils("quick", "binary");
     int target;
     string def = "[ ";
-    for (int elem : arr) {
+    for (int elem: arr) {
         def += to_string(elem);
         def += " ";
     }
@@ -18,7 +19,7 @@ int main(){
     cin >> target;
     int result = searchUtils.search(arr, target);
     string sorted = "[ ";
-    for (int elem : searchUtils.sorted_arr) {
+    for (int elem: searchUtils.sorted_arr) {
         sorted += to_string(elem);
         sorted += " ";
     }
--- utils/sort.cpp
+++ utils/sort.cpp
@@ -4,40 +4,40 @@
 #include "sort.hpp"
 
 using namespace std;
-Sort::Sort(string type): type(std::move(type)){}
+Sort::Sort(string type): type(std::move(type)) {}
 
-vector<int> Sort::sort(vector<int> arr) {
+vector < int > Sort::sort(vector < int > arr) {
     _arr = std::move(arr);
-    if(type == "bubble") return bubbleSort();
-    else if(type == "selection") return selectionSort();
-    else if(type == "insertion") return insertionSort();
-    else if(type == "quick") return quickSort();
-    else if(type == "merge") return mergeSort();
+    if (type == "bubble") return bubbleSort();
+    else if (type == "selection") return selectionSort();
+    else if (type == "insertion") return insertionSort();
+    else if (type == "quick") return quickSort();
+    else if (type == "merge") return mergeSort();
     return _arr;
 };
 
-vector<int> Sort::bubbleSort() {
+vector < int > Sort::bubbleSort() {
     bool swapped;
-    do{
+    do {
         swapped = false;
-        for(int i = 0; i < _arr.size() - 1; i++){
-            if(_arr[i] > _arr[i + 1]){
+        for (int i = 0; i < _arr.size() - 1; i++) {
+            if (_arr[i] > _arr[i + 1]) {
                 int curr = _arr[i];
                 _arr[i] = _arr[i + 1];
                 _arr[i + 1] = curr;
                 swapped = true;
             }
         }
-    }while(swapped);
+    } while (swapped);
     return _arr;
 }
-vector<int> Sort::selectionSort() {
-    for(int i = 0; i < _arr.size(); i++){
+vector < int > Sort::selectionSort() {
+    for (int i = 0; i < _arr.size(); i++) {
         int min = i;
-        for(int j = i + 1; j < _arr.size(); j++){
-            if(_arr[min] > _arr[j]) min = j;
+        for (int j = i + 1; j < _arr.size(); j++) {
+            if (_arr[min] > _arr[j]) min = j;
         }
-        if(min != i){
+        if (min != i) {
             int curr = _arr[i];
             _arr[i] = _arr[min];
             _arr[min] = curr;
@@ -45,70 +45,70 @@ vector<int> Sort::selectionSort() {
     }
     return _arr;
 }
-vector<int> Sort::insertionSort() {
-    for(int i = 1; i < _arr.size(); i++){
+vector < int > Sort::insertionSort() {
+    for (int i = 1; i < _arr.size(); i++) {
         int curr = _arr[i];
         int j = i - 1;
-        while(j > -1 && _arr[j] > curr){
-                _arr[j + 1] = _arr[j];
-                j--;
+        while (j > -1 && _arr[j] > curr) {
+            _arr[j + 1] = _arr[j];
+            j--;
         }
         _arr[j + 1] = curr;
     }
     return _arr;
 }
-vector<int> Sort::quickSort() {
+vector < int > Sort::quickSort() {
     return quickSortFunction(_arr);
 }
-vector<int> Sort::quickSortFunction(vector<int> arr) {
-    if(arr.size() <= 1) return arr;
+vector < int > Sort::quickSortFunction(vector < int > arr) {
+    if (arr.size() <= 1) return arr;
     int pivotIndex = floor(arr.size() / 2);
-    vector<int> left;
-    vector<int> right;
+    vector < int > left;
+    vector < int > right;
 
-    for(int i = 0; i < arr.size(); i++){
-        if(i == pivotIndex) continue;
-        if(arr[i] < arr[pivotIndex]) left.push_back(arr[i]);
+    for (int i = 0; i < arr.size(); i++) {
+        if (i == pivotIndex) continue;
+        if (arr[i] < arr[pivotIndex]) left.push_back(arr[i]);
         else right.push_back(arr[i]);
     }
-    vector<int> leftSorted = quickSortFunction(left);
-    vector<int> rightSorted = quickSortFunction(right);
-    vector<int> merged;
+    vector < int > leftSorted = quickSortFunction(left);
+    vector < int > rightSorted = quickSortFunction(right);
+    vector < int > merged;
     merged.insert(merged.end(), leftSorted.begin(), leftSorted.end());
     merged.push_back(arr[pivotIndex]);
     merged.insert(merged.end(), rightSorted.begin(), rightSorted.end());
     return merged;
 }
-vector<int> Sort::mergeSort() {
+vector < int > Sort::mergeSort() {
     return mergeSortFunction(_arr);
 }
-vector<int> Sort::mergeSortFunction(vector<int> arr) {
-    if(arr.size() <= 1) return arr;
+vector < int > Sort::mergeSortFunction(vector < int > arr) {
+    if (arr.size() <= 1) return arr;
     int mid = floor(arr.size() / 2);
-    vector<int> left(arr.begin(), arr.begin() + mid);
-    vector<int> right(arr.begin() + mid, arr.end());
-    vector<int> leftSort = mergeSortFunction(left);
-    vector<int> rightSort = mergeSortFunction(right);
+    vector < int > left(arr.begin(), arr.begin() + mid);
+    vector < int > right(arr.begin() + mid, arr.end());
+    vector < int > leftSort = mergeSortFunction(left);
+    vector < int > rightSort = mergeSortFunction(right);
     return merge(leftSort, rightSort);
 }
-vector<int> Sort::merge(vector<int> left, vector<int> right) {
-    vector<int> merge;
+vector < int > Sort::merge(vector < int > left, vector < int > right) {
+    vector < int > merge;
     int leftIndex = 0;
     int rightIndex = 0;
-    while(leftIndex < left.size() && rightIndex < right.size()){
-     if(left[leftIndex] >= right[rightIndex]){
-         merge.push_back(right[rightIndex]);
-         rightIndex++;
-     }else {
-         merge.push_back(left[leftIndex]);
-         leftIndex++;
-     }
+    while (leftIndex < left.size() && rightIndex < right.size()) {
+        if (left[leftIndex] >= right[rightIndex]) {
+            merge.push_back(right[rightIndex]);
+            rightIndex++;
+        } else {
+            merge.push_back(left[leftIndex]);
+            leftIndex++;
+        }
     }
-    while(leftIndex < left.size()){
+    while (leftIndex < left.size()) {
         merge.push_back(left[leftIndex]);
         leftIndex++;
     }
-    while(rightIndex < right.size()){
+    while (rightIndex < right.size()) {
         merge.push_back(right[rightIndex]);
         rightIndex++;
     }
